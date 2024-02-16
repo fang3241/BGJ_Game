23c4cbcd7df956eaacbcaa590b6367ed9f948eba
fang3241 <mikhsanmf@gmail.com>
2024-02-16 16:45:54 +0700

    AddMainMenu
    + Main Menu
    + LeanTween(plugin buat animasi)
    + asset UI

---
+++ Brackey_GameJam/Assets/Scripts/ButtonNav.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonNav : MonoBehaviour
+{
+    public GameObject ExitPanel;
+
+    public enum FloorName
+    {
+        //isi nama scene nya
+        MainMenu,
+        
+    };
+
+
+    public void ToMenu()
+    {
+        SceneManager.LoadScene(FloorName.MainMenu.ToString());
+    }
+    
+    public void GameExit()
+    {
+        Application.Quit();
+    }
+
+    public void OpenExitPanel()
+    {
+        ExitPanel.transform.GetChild(0).localScale = new Vector3(0, 0, 0);
+        ExitPanel.SetActive(true);
+        LeanTween.scale(ExitPanel.transform.GetChild(0).gameObject, new Vector3(1, 1, 1), 0.5f);
+        
+    }
+
+    public void CloseExitPanel()
+    {
+        LeanTween.scale(ExitPanel.transform.GetChild(0).gameObject, new Vector3(1.1f, 1.1f, 1.1f), 0.15f).setOnComplete(() =>
+        {
+            LeanTween.scale(ExitPanel.transform.GetChild(0).gameObject, new Vector3(0, 0, 0), 0.50f).setOnComplete(() =>
+            {
+                ExitPanel.SetActive(false);
+            });
+        });
+        
+        
+    }
+}

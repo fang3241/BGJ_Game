791f3b0ad2c91fc5185920427ed7de0dde67b85b
EXOthicc <ahmd.habiballah@gmail.com>
2024-02-18 15:10:18 +0700

    Add gameover

--- Brackey_GameJam/Assets/HpSliderController.cs
+++ Brackey_GameJam/Assets/HpSliderController.cs
@@ -16,6 +16,15 @@ public class HpSliderController : MonoBehaviour
 
     public Color beginColor;
     public Color endColor;
+    bool isLerping = false;
+
+    public GameObject bgred;
+    public GameObject gameOver;
+    public GameObject instantiatedPanel;
+    public Transform canvasTransform;
+    GameObject panelGameOver;
+    public float currentLerpTime;
+    Image bg;
 
     
     public void Setup(PlayerMovement pl, float hp)
@@ -42,6 +51,15 @@ public class HpSliderController : MonoBehaviour
             hpSlider.value = currentHP;
             player.playerHP = currentHP;
         });
+
+        if(currentHP <= 1){
+            Debug.Log("DEADD");
+            //lerp warna layar
+            //show lose panel
+
+            // Start lerping the canvas alpha
+            isLerping = true;
+        }
     }
 
     public void AddHP(float heal)
@@ -52,6 +70,47 @@ public class HpSliderController : MonoBehaviour
               hpSlider.value = currentHP;
               player.playerHP = currentHP;
           });
+    }
+
+    void Start(){
+        instantiatedPanel = Instantiate(bgred, canvasTransform);
+        instantiatedPanel.SetActive(false);
+        panelGameOver = Instantiate(gameOver, canvasTransform);
+        
+        // Get the CanvasGroup component of the instantiated panel
+        bg = panelGameOver.GetComponent<Image>();
+        bg.color = new Color(bg.color.r, bg.color.g, bg.color.b, 0f);
+        panelGameOver.SetActive(false);
+    }
+
+    void Update(){
+        if (isLerping)
+        {
+            // Update the current lerp time
+            currentLerpTime += Time.deltaTime;
+            instantiatedPanel.SetActive(false);
+            panelGameOver.SetActive(true);
+            // Calculate the lerp t value
+            float t = currentLerpTime;
+
+            // Lerp the canvas alpha
+            bg.color = new Color(bg.color.r, bg.color.g, bg.color.b, Mathf.Lerp(0f, 1f, t));
+            //bg.alpha = Mathf.Lerp(startAlpha, targetAlpha, t);
+            Debug.Log("XP");
+            // Check if the lerp is complete
+            if (currentLerpTime >= 1)
+            {
+                Debug.Log("XXP");
+                // Ensure that the alpha reaches exactly 1
+                //bg.alpha = targetAlpha;
+                bg.color = new Color(bg.color.r, bg.color.g, bg.color.b, Mathf.Lerp(0f, 1f, t));
+
+                // Activate the child GameObject of the panel
+                
+                GameObject _player = GameObject.Find("Player(Clone)");
+                _player.SetActive(false);
+            }
+        }
     }
     
 
